65a5d29fbe19224f99275317831c056f638fd384
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-09 14:18:34 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,12 @@
 #cription: Print the alphabet in lowercase and uppercase on same line
 * using putchar only thrice, followed by new line
-  * Return: 0
-   */
+* Return : 0
+/**
+* main - Prints a random number and states whether
+*        it is positive, negative, or zero.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 char alpha = 'a';
