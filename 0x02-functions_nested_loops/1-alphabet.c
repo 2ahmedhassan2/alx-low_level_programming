cc07d625280366a574a1677d5088a38a6c41b414
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-13 09:13:16 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - more headers goes there
+ *
+ * Betty Checker
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar("\n");
+return (0);
+};
