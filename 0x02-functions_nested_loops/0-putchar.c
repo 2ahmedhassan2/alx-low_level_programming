ffa40982498ff54bd3ac2b18ca3e06bffbeb5908
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-13 08:37:49 -0800

    /**

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - more headers goes there
+*
+* Betty Checker
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+putchar('_putchar');
+putchar('\n');
+return (0);
+}
