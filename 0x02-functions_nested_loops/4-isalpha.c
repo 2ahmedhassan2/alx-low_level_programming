e2bdee1ff51e56b9cedc8e4db84f184b8f238070
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-14 08:14:07 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point _putchar
+ * @c: Entry
+ * Return 0 Always
+ */
+int _isalpha(int c);
+{
+if (c >= 'A' && c >= 'a' && <= 'z' && <= 'Z')
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
