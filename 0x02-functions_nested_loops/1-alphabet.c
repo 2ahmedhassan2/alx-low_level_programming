f54836ed86374977680aa1a457a2284522e4a410
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-14 03:18:41 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
- * Return:0
+ * print_alphabet - Entry point _putchar
+ * from a - z
  */
 void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; ++i)
-{
 _putchar(i);
 _putchar('\n');
-}
 }
