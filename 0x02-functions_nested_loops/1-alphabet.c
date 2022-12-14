9f257df9d83055331f649f283417d3f4b427e878
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-14 01:50:38 -0800

    main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
  * print_alphabet - Entry point
  * Return:0
  */
-void print_alphabet(c)
+void print_alphabet(void)
 {
-int c;
 char c = 'a';
 while (c <= 'z')
 {
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int main(void);
+void ptint_alphabet(void);
+
+
+
+
+
+
+
+
+#endif
