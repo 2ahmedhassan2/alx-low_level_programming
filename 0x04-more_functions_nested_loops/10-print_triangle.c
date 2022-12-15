56921494af1b5bff079157b330fd2b67c94ad352
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-15 13:22:10 -0800

    #include "main.h"

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,54 +1,29 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-* print_triangle - prints triangle
-* @size: parameter
-* Return: returns nothing
-*/
+ * print_triangle - create triangle
+ * @size: params
+ * Return: 0
+ */
 
-void print_square(int size)
+void print_triangle(int size)
 {
-int inc1, inc2;
+int i = 0, j, n = size - 1;
 if (size > 0)
 {
-for (inc1 = 1; inc1 <= size; inc1++)
+for (; i < size; i++)
 {
-for ((inc2 = size - 1); inc2 > 0; inc2--)
+for (j = 0; j < size; j++)
 {
-putchar(' ');
+if (j < n)
+_putchar(' ');
+else
+_putchar('#');
 }
-putchar('#');
-putchar('\n');
+n--;
+_putchar('\n');
 }
 }
 else
-{
-putchar('\n');
-=======
-* print_triangle - prints a triangle, followed by a new line
-* @size: size of the triangle
-*/
-void print_triangle(int size)
-{
-if (size <= 0)
-{
 _putchar('\n');
-} else
-{
-int i, j;
-for (i = 1; i <= size; i++)
-{
-for (j = i; j < size; j++)
-{
-_putchar(' ');
-}
-for (j = 1; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
->>>>>>> cbe2a03add149a2e1f86df0e7b777490848dba3a
-}
 }
