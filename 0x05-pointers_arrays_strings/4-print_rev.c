817652e6a34a76df3229d9977378a7b576519b6d
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-19 07:49:29 -0800

    int i = 0;

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - main
+ * @s: parameter
+ * Return: Always 0
+ *
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+i -= 1;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+};
+_putchar('\n');
+}
