7cfbd6e95c17dbbe9a78b2b3d64058fbe604f25b
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-19 04:03:36 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - main
+ * @n: parameter
+ * Return: Always 0
+ *
+ */
+
+void reset_to_98(int *n);
+{
+int *n = 98
+_putchar(',n');
+return (0);
+}
+
+
+
+
+
