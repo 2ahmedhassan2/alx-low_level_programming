bd35f2d4676fd18ce9672976166b4ef692008806
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-19 04:37:11 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - main
+ * @s: parameter
+ * Return: Always 0
+ *
+ */
+
+int _strlen(char *s)
+{
+*s = "How";
+_strlen(char *s)
+}
