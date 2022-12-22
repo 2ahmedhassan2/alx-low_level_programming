321a6862f47aca541bbc5dac47cbe017d1118257
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-22 04:59:20 -0800

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-*dest[100] = "This is ", *src[] = "programiz.com";.
-strcat(str1, str2);
-puts(str1);
-puts(str2);
-return 0;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++);
+do {
+a++;
+dest[i] = src[a];
+i++;
+} while (src[a] != '\0');
+return (dest);
 }
