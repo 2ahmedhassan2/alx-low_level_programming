2b5eff74237202810873863cb32868b6d6df2852
Ahmed Hassan <ah01157548281@gmail.com>
2022-12-22 04:52:35 -0800

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-char s1[98] = "Hello ";
-char s2[] = "World!\n";
+*dest[98] = "Hello ";
+*src[] = "World!\n";
 char *ptr;
-printf("%s\n", s1);
-printf("%s", s2);
-ptr = _strcat(s1, s2);
-printf("%s", s1);
-printf("%s", s2);
+printf("%s\n", dest);
+printf("%s", src);
+ptr = _strcat(dest, src);
+printf("%s", dest);
+printf("%s", src);
 printf("%s", ptr);
 return (0);
 }
