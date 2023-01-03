3855c9cff775e376ee3221bebc324a2e389f757c
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-03 03:28:43 -0800

    3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - fills the first n bytes of the memory area
+ * @s: pointer to the memory area to be filled
+ * @: constant byte to fill the memory area
+ * @c: number of bytes to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+}
