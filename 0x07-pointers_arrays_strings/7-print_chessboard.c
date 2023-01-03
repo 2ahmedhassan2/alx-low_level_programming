ecccfdd3f673944bec4a54e188feaa50f839cea3
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-03 09:37:10 -0800

    6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,14 +9,10 @@
 
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-printf("%c ", a[i][j]);
+int sum1 = 0, sum2 = 0;
+for (int i = 0; i < size; i++) {
+sum1 += a[i * size + i];
+sum2 += a[i * size + (size - i - 1)];
 }
-printf("\n");
-}
+printf("Sum of the two diagonals: %d\n", sum1 + sum2);
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,19 +11,10 @@
 
 void print_diagsums(int *a, int size)
 {
-int i;
 int sum1 = 0, sum2 = 0;
-for (i = 0; i < size; i++)
-{
+for (int i = 0; i < size; i++) {
 sum1 += a[i * size + i];
-sum2 += a[i * size + (size - 1 - i)];
+sum2 += a[i * size + (size - i - 1)];
 }
-printf("%d %d\n", sum1, sum2);
-int matrix[3][3] = {
-{1, 2, 3},
-{4, 5, 6},
-{7, 8, 9}
-};
-
-print_diagsums((int *)matrix, 3);
+printf("Sum of the two diagonals: %d\n", sum1 + sum2);
 }
