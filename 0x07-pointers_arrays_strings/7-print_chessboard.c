5f2cbebf0aa3a2800fd272736d23e3f270bf7714
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-03 09:31:51 -0800

    6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,13 @@
 /**
  * print_chessboard - fills the first n bytes of the memory area
  * @a: pointer to the memory area to be filled
- * @: constant byte to fill the memory area
- * @: number of bytes to be filled
  * Return: pointer to the memory area s
  */
 
 void print_chessboard(char (*a)[8])
 {
 int i, j;
+
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
