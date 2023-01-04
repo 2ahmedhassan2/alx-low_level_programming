a1cb1c93a6aaac231dddf1020815d42d20275fbc
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-04 13:12:09 -0800

    3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - checks uppercase
+ * @s: parameter
+ * Return: 0 if sucessful
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+}
