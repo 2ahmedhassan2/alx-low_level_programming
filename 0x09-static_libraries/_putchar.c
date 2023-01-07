912b23810430010f8f246732ff705dea98d21977
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-07 09:12:11 -0800

    1

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "main.h"
-/**
-* main - more headers goes there
-*
-* Betty Checker
-*
-* Return: Always 0 (Success)
-*/
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-printf("_putchar");
-printf("\n");
-return (0);
+return (write(1, &c, 1));
 }
