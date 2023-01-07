cd07bc2ceccd5d78f1b9dee6f93e9cac004280b9
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-07 13:03:51 -0800

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - fills the first n bytes of the memory area
- * @arg: pointer to the memory area to be filled
+ * @argc: pointer to the memory area to be filled
  * @argv: constant byte to fill the memory area
  * @: number of bytes to be filled
  * Return: pointer to the memory area s
  */
 
-int main(int arg, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
