d72cfc9540506a926e57f9e7b9313deb3ec9f939
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-07 13:00:44 -0800

    2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: pointer to the memory area s
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
