123a381bb636de5244f9e99a61626b3733a88ceb
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-07 09:04:18 -0800

    2

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+putchar('\n');
