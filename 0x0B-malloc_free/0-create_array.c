350fd0607408ef9bb3b4d0df67e4abed146b6ae1
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-09 06:47:09 -0800

    1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include <stdlib>
+#include "main.h"
+
+/**
+ * create_array - checks uppercase
+ * @size: parameter
+ * @c: parameter
+ * Return: Null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+return (NULL);
+}
