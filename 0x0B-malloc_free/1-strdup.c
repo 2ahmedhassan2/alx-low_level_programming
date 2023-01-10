627277bcc4afa5eea33d0017a0fc0767692791a1
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-10 04:56:32 -0800

    2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,5 +11,13 @@
 
 char *_strdup(char *str)
 {
-
+*str = _strdup("ALX SE");
+if (s == NULL)
+{
+printf("failed to allocate memory\n");
+return (1);
+}
+printf("%s\n", str);
+free(str);
+return (0);
 }
