6a6985940a35495f90f50184d779451b242ce175
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-10 14:32:15 -0800

    Final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,33 +3,63 @@
 #include <stdlib.h>
 
 /**
- * strtow - checks uppercase
- * @str: parameter
- * @: parameter
- * Return: As function shows below
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
  */
-
+void ch_free_grid(char **grid, unsigned int height)
+{
+if (grid != NULL && height != 0)
+for (; height > 0; height--)
+free(grid[height]);
+free(grid[height]);
+free(grid);
+}
+}
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
 char **strtow(char *str)
 {
-char **newstr;
-int i = 0, j = 0, str_len = 0, words;
-if (str == NULL || str[0] == '\0')
+char **aout;
+unsigned int c, height, i, j, a1;
+if (str == NULL || *str == '\0')
 return (NULL);
-while (*(str + str_len) != '\0')
-str_len++;
-words = word_count(str, str_len);
-if (!words)
+for (c = height = 0; str[c] != '\0'; c++}
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+height++;
+aout = malloc((height + 1) * sizeof(char *));
+if (aout == NULL || height == 0)
+{
+free(aout);
 return (NULL);
-newstr = malloc((words + 1) * sizeof(mallocmem(newstr, str, str_len);
-while (i < str_len)
-{
-if (str[i] != ' ')
-{
-i = strncat_mod(newstr[j], str, i, str_len);
-j++, i--;
 }
-i++;
+for (i = a1 = 0; i < height; i++)
+{
+for (c = a1; str[c] != '\0'; c++)
+{
+if (str[c] == ' ')
+a1++;
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+{
+aout[i] = malloc((c - a1 + 2) * sizeof(char));
+if (aout[i] == NULL)
+{
+ch_free_grid(aout, i);
+return (NULL);
 }
-newstr[words + 1] = NULL;
-return (newstr);
+break;
+}
+}
+for (j = 0; a1 <= c; a1++, j++)
+aout[i][j] = str[a1];
+aout[i][j] = '\0';
+}
+aout[i] = NULL;
+return (aout);
 }
