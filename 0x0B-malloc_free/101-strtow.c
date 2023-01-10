2cf05a57f65006b8b130b3a85bdec6719af5ca26
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-10 14:17:22 -0800

    Final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,5 +11,25 @@
 
 char **strtow(char *str)
 {
-
+char **newstr;
+int i = 0, j = 0, str_len = 0, words;
+if (str == NULL || str[0] == '\0')
+return (NULL);
+while (*(str + str_len) != '\0')
+str_len++;
+words = word_count(str, str_len);
+if (!words)
+return (NULL);
+newstr = malloc((words + 1) * sizeof(mallocmem(newstr, str, str_len);
+while (i < str_len)
+{
+if (str[i] != ' ')
+{
+i = strncat_mod(newstr[j], str, i, str_len);
+j++, i--;
+}
+i++;
+}
+newstr[words + 1] = NULL;
+return (newstr);
 }
