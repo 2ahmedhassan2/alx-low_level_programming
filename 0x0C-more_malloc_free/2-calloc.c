8eb0dfca0d14874ce26f48a178fa6ed21289b042
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-13 14:05:14 -0800

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
-if (nmemb == 0 || size == 0) {
+if (nmemb == 0 || size == 0)
+{
 return (NULL);
 }
 ptr = malloc(nmemb * size);
-if (ptr == NULL) {
+if (ptr == NULL)
+{
 return (NULL);
 }
 memset(ptr, 0, nmemb * size);
