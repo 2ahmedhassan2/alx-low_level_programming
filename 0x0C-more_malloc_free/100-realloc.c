81779d11d274c5e2cd32600aab4e71ea9a8577cd
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-14 03:48:34 -0800

    5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,23 +21,16 @@ if (new_size == 0 && ptr != NULL)
 free(ptr);
 return (NULL);
 }
-if (ptr == NULL)
-{
-return (malloc(new_size));
-}
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 {
 return (NULL);
 }
-if (new_size > old_size)
+if (ptr != NULL)
 {
-memcpy(new_ptr, ptr, old_size);
-}
-else
-{
-memcpy(new_ptr, ptr, new_size);
-}
+size_t copy_size = (old_size < new_size) ? old_size : new_size;
+memcpy(new_ptr, ptr, copy_size);
 free(ptr);
+}
 return (new_ptr);
 }
