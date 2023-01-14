c8d979e6b679e9e71f0e919d506dc20390fbcf6d
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-14 06:40:47 -0800

    6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,18 +5,6 @@
 #include <main.h>
 
 /**
- * multiply - checks uppercase
- * @num1: parameter
- * @num2: parameter
- * Return: Null
- */
-
-long long multiply(char *num1, char *num2)
-{
-return (atoll(num1) * atoll(num2));
-}
-
-/**
  * is_valid_number - checks uppercase
  * @num: parameter
  * @: parameter
@@ -26,7 +14,8 @@ return (atoll(num1) * atoll(num2));
 int is_valid_number(char *num)
 {
 int i;
-for (i = 0; i < strlen(num); i++)
+int len = strlen(num);
+for (i = 0; i < len; i++)
 {
 if (!isdigit(num[i]))
 {
@@ -34,6 +23,18 @@ return (0);
 }
 }
 return (1);
+}
+
+/**
+ * multiply - checks uppercase
+ * @num1: parameter
+ * @num2: parameter
+ * Return: Null
+ */
+
+long long multiply(char *num1, char *num2)
+{
+return (atoll(num1) * atoll(num2));
 }
 
 /**
@@ -48,13 +49,13 @@ int main(int argc, char *argv[])
 if (argc != 3)
 {
 printf("Error\n");
-exit(98);
+exit(EXIT_FAILURE);
 }
 if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
 {
 printf("Error\n");
-exit(98);
+exit(EXIT_FAILURE);
 }
 printf("%lld\n", multiply(argv[1], argv[2]));
-return (0);
+return (EXIT_SUCCESS);
 }
