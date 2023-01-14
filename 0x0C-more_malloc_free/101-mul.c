ee770a563d9bfce681a42d665e3eaaf2803120a0
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-14 07:00:51 -0800

    6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,21 +9,39 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
+long double mul;
 int i, j;
+char *end;
 if (argc != 3)
-{ printf("Error\n");
-exit(98); }
+{
+printf("Error: Invalid number of arguments\n");
+exit(98);
+}
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{  printf("Error\n");
-exit(98); }
+if (!isdigit(argv[i][j]) && argv[i][j] != '.')
+{
+printf("Error: Invalid argument\n");
+exit(98);
 }
 }
-mul = atol(argv[1]) * atol(argv[2]);
-printf("%lu\n", mul);
+}
+errno = 0;
+mul = strtold(argv[1], &end);
+if (errno == ERANGE || *end != '\0')
+{
+printf("Error: Invalid argument\n");
+exit(98);
+}
+errno = 0;
+mul *= strtold(argv[2], &end);
+if (errno == ERANGE || *end != '\0')
+{
+printf("Error: Invalid argument\n");
+exit(98);
+}
+printf("%.10Lf\n", mul);
 return (0);
 }
