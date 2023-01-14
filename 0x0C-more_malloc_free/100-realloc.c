ca00bc954fc4595549f82bbe599f57bdca7be90f
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-14 03:55:47 -0800

    5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,26 +10,29 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr;
+char *nptr;
+unsigned int i;
 if (new_size == old_size)
-{
 return (ptr);
-}
-if (new_size == 0 && ptr != NULL)
+if ((new_size == 0) && (ptr != NULL))
 {
 free(ptr);
 return (NULL);
 }
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
+if (ptr == NULL)
 {
+nptr = malloc(new_size);
+if (nptr == NULL)
 return (NULL);
 }
-if (ptr != NULL)
+if (new_size > old_size && (ptr != NULL))
 {
-size_t copy_size = (old_size < new_size) ? old_size : new_size;
-memcpy(new_ptr, ptr, copy_size);
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (nptr);
+for (i = 0; i < old_size; i++)
+nptr[i] = *((char *) ptr + 1);
 free(ptr);
 }
-return (new_ptr);
+return (nptr);
 }
