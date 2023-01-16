b1aca4749107124aca2e2c097a108fdb03186a97
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 14:14:30 -0800

    3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @void: takes no params
+ *
+ * Return: always 0 if successful:
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
