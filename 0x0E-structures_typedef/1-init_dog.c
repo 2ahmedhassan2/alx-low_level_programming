1654d12a8d814c482795268c2025df7fc5895606
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 04:47:43 -0800

    2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - checks uppercase
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: Null
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct 
+}
