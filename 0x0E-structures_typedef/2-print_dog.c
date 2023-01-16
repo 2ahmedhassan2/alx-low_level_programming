4850a257ac96f701737ea7a804a86473adca190a
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 08:03:26 -0800

    3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - checks uppercase
+ * @d: parameter
+ * Return: Null
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return ("nil");
+}
+d.name = "Poppy";
+d.age = 3.5;
+d.owner = "Bob";
+print_dog(&d);
+if (name == NULL)
+{
+return ("Name = (nil)");
+}
+return (0);
+}
