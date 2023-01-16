4ff5c1c3f182449b4b93b0910d30b8a2e7ee9fff
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 04:20:16 -0800

    All

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,30 @@
-include <stdio.h>
-include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * main - checks uppercase
+ * @: parameter
+ * @: parameter
+ * Return: 0
+ */
+
+int main(void)
+{
+/**
+ * struct dog - struct dog function
+ * This struct stores information about a dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+return (0);
+}
+
+#endif
