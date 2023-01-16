f7b5c46c1ae005f52ccc8e61a32f56e2e3a088d7
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 05:13:12 -0800

    2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog *d
-{
-char *name;
-float age;
-char *owner; 
-} d;
+d->name = name;
+d->age = age;
+d->owner = owner; 
 }
