f6f01cf359f3c9d9fe3f982e4f57142f93343135
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-16 07:26:07 -0800

    3

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,14 +2,12 @@
 #include "dog.h"
 
 /**
- * init_dog - checks uppercase
- * @d: parameter
- * @name: parameter
- * @age: parameter
- * @owner: parameter
- * Return: Null
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
