2ceaf0feeff8624df5a98bb15696de12ee0a4a99
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-18 07:32:24 -0800

    2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,14 +11,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (size <= 0) {
-return -1;
+if (size <= 0)
+{
+return (-1);
 }
 int i;
-for (i = 0; i < size; i++) {
-if (cmp(array[i]) != 0) {
-return i;
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
 }
 }
-return -1;
+return (-1);
 }
