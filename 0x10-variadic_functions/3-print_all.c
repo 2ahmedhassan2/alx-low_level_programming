098e9bc9d30ddb9542a81e4f10d11b05ed00de44
Ahmed Hassan <ah01157548281@gmail.com>
2023-01-19 14:11:15 -0800

    4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,73 +1,44 @@
 #include "variadic_functions.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdarg.h>
 /**
- * print_int - prints int
- * @list: arguments from print_all
- */
-void print_int(va_list list)
-{
-printf("%d", va_arg(list, int));
-}
-/**
- * print_float - prints float
- * @list: arguments from print_all
- */
-void print_float(va_list list)
-{
-printf("%f", va_arg(list, double));
-}
-/**
- * print_char - prints int
- * @list: arguments from print_all
- */
-void print_char(va_list list)
-{
-printf("%c", va_arg(list, int));
-}
-/**
- * print_str - prints string
- * @list: arguments from print_all
- */
-void print_str(va_list list)
-{
-char *s = va_arg(list, char *);
-s == NULL ? printf("(nil)") : printf("%s", s);
-}
-/**
- * print_all - prints any type
- * @format: arguments to print
+ * print_all - prints int
+ * @format: arguments from print_all
  */
 void print_all(const char * const format, ...)
 {
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
-printTypeStruct printType[] = {
-{ "i", print_int },
-{ "f", print_float },
-{ "c", print_char },
-{ "s", print_str },
-{NULL, NULL}
-};
-va_start(list, format);
-while (format && format[i])
+va_list args;
+va_start(args, format);
+int i = 0;
+char c;
+while ((c = format[i]) != '\0')
 {
-j = 0;
-while (j < 4)
+if (c == 'c')
 {
-if (*printType[j].type == format[i])
-{
-printf("%s", sep);
-printType[j].printer(list);
-sep = ", ";
-break;
+printf("%c", va_arg(args, int));
 }
-j++;
+else if (c == 'i')
+{
+printf("%d", va_arg(args, int));
+}
+else if (c == 'f')
+{
+printf("%f", va_arg(args, double));
+}
+else if (c == 's')
+char *s = va_arg(args, char*);
+if (s == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", s);
+}
 }
 i++;
 }
+va_end(args);
 printf("\n");
-va_end(list);
 }
