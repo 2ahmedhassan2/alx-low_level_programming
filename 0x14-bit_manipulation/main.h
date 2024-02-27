dfb3308998a46081c7850052c5656c0d21c7c36b
Ahmed Hassan <ah01157548281@gmail.com>
2024-02-27 12:16:43 -0800

    dd

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,8 @@
-#ifndef BIT_MANIPULATION
-#define BIT_MANIPULATION
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <limits.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
