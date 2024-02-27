170048d3eb47b171417ed5a0677e5af2fd7e0a42
Ahmed Hassan <ah01157548281@gmail.com>
2024-02-27 08:52:30 -0800

    2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,31 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int slen = 0, i;
-	unsigned int total = 0;
+    int slen = 0, i;
+    unsigned int total = 0;
 
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+        return (0);
 
-	while (b[slen] != '\0')
-	{
-		slen++;
-	}
-	slen -= 1;
-	i = 0;
+    while (b[slen] != '\0')
+    {
+        if (b[slen] != '0' && b[slen] != '1')
+            return (0); // Return 0 if any character is not '0' or '1'
+        slen++;
+    }
+    slen -= 1;
+    i = 0;
 
-	while (b[i])
-	{
-		if ((b[i] != '0') && (b[i] != '1'))
-		{
-			return (total);
-		}
-		if (b[i] == '1')
-		{
-			total += (1 * (1 << slen));
-		}
-		i++;
-		slen--;
-	}
-	return (total);
+    while (b[i])
+    {
+        if (b[i] == '1')
+        {
+            total += (1 * (1 << slen));
+        }
+        i++;
+        slen--;
+    }
+    return (total);
 }
