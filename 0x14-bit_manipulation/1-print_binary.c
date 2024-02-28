7bef48b402f5afa6ec17687042d149af79ade7e3
Ahmed Hassan <ah01157548281@gmail.com>
2024-02-28 06:02:47 -0800

    sds

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,34 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to print in binary.
+ * print_binary - prints binary representation of a number
+ * @n: unsigned int
+ * Return: Binary rep
  */
 void print_binary(unsigned long int n)
 {
-/* Base case: If n is 0, print 0 and return */
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-/* Initialize a mask with the highest bit set */
-unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-/* Find the position of the most significant bit */
-while ((mask & n) == 0)
-{
-mask >>= 1;
-}
-/* Print binary representation */
-while (mask != 0)
-{
-if (mask & n)
-_putchar('1');
-else
-_putchar('0');
-mask >>= 1;
-}
+	unsigned long int n1 = n, mask = 1;
+	int slen = 0; /*string length*/
+
+	while (n1 > 0)
+	{
+		slen++;
+		n1 >>= 1;
+	}
+	slen -= 1;
+
+	if (slen > 0)
+		mask = mask << slen;
+
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+		}
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
 }
